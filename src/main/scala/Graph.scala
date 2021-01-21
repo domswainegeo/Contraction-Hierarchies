82aee88a7a86b88296fbf0182aa64ac90542e5d9
domswaine <dominic.swaine@gmail.com>
2021-01-21 22:45:33 +0000

    Move incoming, outgoing edges collation to Graph class

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,1 +1,19 @@
-class Graph(val nodes: List[Node], val edges: List[Edge]){}
+class Graph(val nodes: List[Node], val edges: List[Edge]){
+  private var incoming: Option[Map[Node, Array[Edge]]] = None
+  private var outgoing: Option[Map[Node, Array[Edge]]] = None
+
+  def get_incoming(): Map[Node, Array[Edge]] = {
+    if(incoming.isEmpty){
+      incoming = Some(edges.groupBy(edge => edge.b).map{case a -> b => a -> b.toArray})
+    }
+    incoming.get
+  }
+
+  def get_outgoing(): Map[Node, Array[Edge]] = {
+    if(outgoing.isEmpty){
+      outgoing = Some(edges.groupBy(edge => edge.a).map{case a -> b => a -> b.toArray})
+    }
+    outgoing.get
+  }
+
+}
